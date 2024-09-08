e8d2af9eb07a1a2fd01c8648a311915c82867e2c
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-09-08 04:18:28 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('.nav-menu');
     
-    menuToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
-    });
+    if (menuToggle && navMenu) {
+        menuToggle.addEventListener('click', function() {
+            navMenu.classList.toggle('active');
+        });
+    }
 });
